0afd7480de0f2898fd7d5496ba17ff1759e08843
shaye <sm0548540902@gmail.com>
2026-02-12 11:48:55 +0200

    add finish basics

--- src/terorists/terorists.jsx
+++ src/terorists/terorists.jsx
@@ -8,6 +8,16 @@ const Terorists = () => {
     const [byName, setByName] = useState("")
     const [showAll, setShowAll] = useState(true)
     const [byAttacts, seTbyattacts] = useState("")
+    const [byStatus, setBystatus] = useState("all")
+    const [byMostDasngers, setMostDangers] = useState(false)
+    let higeAttacts = 0
+    for(let item of data){
+        if(item.attacksCount > higeAttacts){
+            higeAttacts = item.attacksCount
+        }
+    }
+    console.log(higeAttacts);
+    
     return (
         <div>
             <div>
@@ -17,9 +27,18 @@ const Terorists = () => {
                 <input onClick={() => {
                     setShowAll(false)
                 }} type="text" placeholder='Search by atacts' value={byAttacts} onChange={(e) => seTbyattacts(e.target.value)} />
-                    <select onClick={() => {
+                <select onClick={() => {
+                }} defaultValue={"all"} value={byStatus} onChange={(e) => setBystatus(e.target.value)} >
+                    <option value="all">all</option>
+                    <option value="active">active</option>
+                    <option value="quiet">quiet</option>
+                    <option value="agent">agent</option>
+                    <option value="dead">dead</option>
+                </select>
+                <button onClick={() => {
                     setShowAll(false)
-                }}value={byAttacts} onChange={(e) => seTbyattacts(e.target.value)} ></select>
+                    setMostDangers(true)
+                }}>find most Dangerous</button>
             </div>
             <div id='allTerorist'>
                 <div id='linefirst'>
@@ -30,7 +49,7 @@ const Terorists = () => {
                     <p>Summary</p>
                 </div>
 
-                {showAll && data.map((item) => {
+                {byStatus == "all" && showAll && data.map((item) => {
                     return (
                         <Terorist name={item.name} organization={item.organization} attacks={item.attacksCount} status={item.status} summary={item.relationToIsraelSummary} img={item.imageUrl} />
                     )
@@ -42,6 +61,18 @@ const Terorists = () => {
                         )
                     })}
                 {data.filter((item) => item.attacksCount == byAttacts)
+                    .map((item2) => {
+                        return (
+                            <Terorist name={item2.name} organization={item2.organization} attacks={item2.attacksCount} status={item2.status} summary={item2.relationToIsraelSummary} img={item2.imageUrl} />
+                        )
+                    })}
+                {data.filter((item) => item.status == byStatus)
+                    .map((item2) => {
+                        return (
+                            <Terorist name={item2.name} organization={item2.organization} attacks={item2.attacksCount} status={item2.status} summary={item2.relationToIsraelSummary} img={item2.imageUrl} />
+                        )
+                    })}
+                {byMostDasngers && data.filter((item) => item.status == "active" && item.imageUrl != null && item.attacksCount == higeAttacts)
                     .map((item2) => {
                         return (
                             <Terorist name={item2.name} organization={item2.organization} attacks={item2.attacksCount} status={item2.status} summary={item2.relationToIsraelSummary} img={item2.imageUrl} />
